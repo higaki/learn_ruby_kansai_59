a726c09f06ac8e7f8631d2eb36b77a2d8f099127
HIGAKI Masaru <mas.higa@gmail.com>
2013-12-22 11:59:04 +0900

    Added the test cases for the String#cmarbdige for the string of 1, 2 and 3 characters

--- test_cambridge.rb
+++ test_cambridge.rb
@@ -7,4 +7,16 @@ class TestCambridge < Test::Unit::TestCase
   def test_cmarbdige_empty
     assert_equal("", "".cmarbdige)
   end
+
+  def test_cmarbdige_1character
+    assert_equal("x", "x".cmarbdige)
+  end
+
+  def test_cmarbdige_2characters
+    assert_equal("xy", "xy".cmarbdige)
+  end
+
+  def test_cmarbdige_3characters
+    assert_equal("xyz", "xyz".cmarbdige)
+  end
 end
